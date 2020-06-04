1f8fb9d56fbe400d38e8efe76e7cbeea1432272a
Ming-Kai Jiau (AP15) <kasimjm7997@gmail.com>
2020-06-05 07:17:25 +0800

    feat: add setReleasTime for data in influxdb

--- taifex_scraper/pipelines.py
+++ taifex_scraper/pipelines.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 import requests
+import datetime
+import pytz
 from .utils import make_influx_line
+
+# As offical data from taifex is released at 15:00 afterwards
+# we select 15:00 Taipei time as data point's timestamp.
+def setReleasTime(dt):
+    return datetime.datetime.strptime(dt, '%Y/%m/%d') \
+            .replace(hour=15, tzinfo=pytz.timezone('Asia/Taipei')) \
+            .isoformat()
 
 class TaifexScraperPipeline(object):
     def process_item(self, item, spider):
-        data = make_influx_line('dlPcRatioDown', {}, item, item['日期'])
-        requests.post('http://localhost:9001/telegraf', data=data.encode('utf-8'))
+        # data = make_influx_line('dlPcRatioDown', {}, item, item['日期'])
+        data = make_influx_line('dlPcRatioDown', {}, item, setReleasTime(item['日期']))
+        requests.post('http://host.docker.internal:9001/telegraf', data=data.encode('utf-8'))
+        # requests.post('http://localhost:9001/telegraf', data=data.encode('utf-8'))
         return item

fbf033edd3d62baa8c0a9ea43dcb3ce5c898d575
Ming-Kai Jiau (AP15) <kasimjm7997@gmail.com>
2020-06-06 21:19:10 +0800

    feat: implement two modes of _month_range_requests and _selected_date_request

--- taifex_scraper/spiders/dlPcRatioDown.py
+++ taifex_scraper/spiders/dlPcRatioDown.py
@@ -2,56 +2,27 @@
 from scrapy import FormRequest, Item, Field, Request, Spider
 from scrapy.spiders import CSVFeedSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 import urllib
 import pandas as pd
 import io
 from taifex_scraper.items import *
 from ..utils import first_date_of_month, last_date_of_month
 
-
-my_bias_day = 0
-tx_contract_month = "202006"
-
-start_date = datetime.datetime.today() - timedelta(days=30) - timedelta(days=my_bias_day)
-end_date = datetime.datetime.today() - timedelta(days=my_bias_day)
-
-data = {
-    'queryStartDate': start_date.strftime('%Y/%m/%d'), # '2019/03/20'
-    'queryEndDate': end_date.strftime('%Y/%m/%d')      # '2019/04/19'
-}
-
 class DlpcratiodownSpider(Spider):
     name = 'dlPcRatioDown'
     allowed_domains = ['taifex.com.tw']
-    # start_urls = [SEARCH_QUERY]
 
-    # def start_requests(self):
-    #     yield FormRequest("https://www.taifex.com.tw/cht/3/dlPcRatioDown",
-    #                                formdata=data,
-    #                                callback=self.cb)
-    # def start_requests(self):
-    #     yield Request(url='https://www.taifex.com.tw/cht/3/dlPcRatioDown', 
-    #                                 method='POST', 
-    #                                 callback=self.parse_data,
-    #                                 headers={'Content-Type': 'application/x-www-form-urlencoded'},
-    #                                 body=urllib.parse.urlencode(data, doseq=True))
     def start_requests(self):
-        # print(self.arg1) # scrapy crawl taifex -a arg1=my-string-1
-        month_reqs = []
-        for m in pd.date_range('2010/1','2010/1', freq='MS').strftime("%Y/%m").tolist():
-            data2 = {
-                'queryStartDate': first_date_of_month(m).strftime('%Y/%m/%d'), # '2019/03/20'
-                'queryEndDate': last_date_of_month(m).strftime('%Y/%m/%d')      # '2019/04/19'
-            }
-            req = Request(url='https://www.taifex.com.tw/cht/3/dlPcRatioDown', 
-                            method='POST', 
-                            callback=self.parse_data,
-                            headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                            body=urllib.parse.urlencode(data2, doseq=True))
-            month_reqs.append(req)
-        return month_reqs
+        # scrapy crawl -a start_month="2010/12" -a end_month="2011/3" dlPcRatioDown
+        # scrapyd-client schedule --arg start_month="2010/12" --arg end_month="2011/3" -p taifex_scraper dlPcRatioDown
+        if hasattr(self, 'start_month') and hasattr(self, 'end_month'):
+            return self._month_range_requests(self.start_month, self.end_month)
+
+        # scrapy crawl -a selected_date="2020/06/02" dlPcRatioDown
+        # scrapyd-client schedule --arg selected_date="2020/06/02" -p taifex_scraper dlPcRatioDown
+        selected_date = self.selected_date if hasattr(self, 'selected_date') else datetime.now().strftime('%Y/%m/%d')
+        return self._selected_date_request(selected_date)
     
     def parse_data(self, response):
 
@@ -69,3 +40,31 @@ class DlpcratiodownSpider(Spider):
         items = df.to_dict(orient='records')
         
         return items
+
+    def _month_range_requests(self, start_month, end_month):
+        reqs = []
+
+        for m in pd.date_range(start_month, end_month, freq='MS').strftime("%Y/%m").tolist():
+            data = {
+                'queryStartDate': first_date_of_month(m).strftime('%Y/%m/%d'), # '2019/03/20'
+                'queryEndDate': last_date_of_month(m).strftime('%Y/%m/%d')     # '2019/04/19'
+            }
+            req = Request(url='https://www.taifex.com.tw/cht/3/dlPcRatioDown', 
+                            method='POST', 
+                            callback=self.parse_data,
+                            headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                            body=urllib.parse.urlencode(data, doseq=True))
+            reqs.append(req)
+        return reqs
+
+    def _selected_date_request(self, selected_date):
+        data = {
+            'queryStartDate': selected_date, # '2020/6/3'
+            'queryEndDate': selected_date    # '2019/6/3'
+        }
+        req = Request(url='https://www.taifex.com.tw/cht/3/dlPcRatioDown', 
+                        method='POST', 
+                        callback=self.parse_data,
+                        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                        body=urllib.parse.urlencode(data, doseq=True))
+        return [req]

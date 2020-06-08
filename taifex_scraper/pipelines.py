64f94384f57f15753cdc8f7cf243cf4de6ecb858
Ming-Kai Jiau (AP15) <kasimjm7997@gmail.com>
2020-06-09 07:27:45 +0800

    feat: implement dlFutDataDown and fix setReleasTime's time error

--- taifex_scraper/pipelines.py
+++ taifex_scraper/pipelines.py
@@ -7,9 +7,8 @@ from .utils import make_influx_line
 # As offical data from taifex is released at 15:00 afterwards
 # we select 15:00 Taipei time as data point's timestamp.
 def setReleasTime(dt):
-    return datetime.datetime.strptime(dt, '%Y/%m/%d') \
-            .replace(hour=15, tzinfo=pytz.timezone('Asia/Taipei')) \
-            .isoformat()
+    _dt = datetime.datetime.strptime(dt, '%Y/%m/%d')
+    return pytz.timezone('Asia/Taipei').localize(_dt).replace(hour=15).isoformat()
 
 class TaifexScraperPipeline(object):
     def process_item(self, item, spider):
@@ -21,8 +20,31 @@ class TaifexScraperPipeline(object):
             return item
 
         if spider.name == 'dlFutDataDown':
-            data = make_influx_line('dlFutDataDown', {}, item, setReleasTime(item['交易日期']))
+            tag_set = {
+                "契約": item["契約"],
+                "到期月份(週別)": item["到期月份(週別)"],
+                "是否因訊息面暫停交易": item["是否因訊息面暫停交易"],
+                "交易時段": item["交易時段"]
+            }
+            field_set = {
+                "開盤價": item["開盤價"],
+                "最高價": item["最高價"],
+                "最低價": item["最低價"],
+                "收盤價": item["收盤價"],
+                "漲跌價": item["漲跌價"],
+                "漲跌%": item["漲跌%"],
+                "成交量": item["成交量"],
+                "結算價": item["結算價"],
+                "未沖銷契約數": item["未沖銷契約數"],
+                "最後最佳買價": item["最後最佳買價"],
+                "最後最佳賣價": item["最後最佳賣價"],
+                "歷史最高價": item["歷史最高價"],
+                "歷史最低價": item["歷史最低價"],
+                "價差對單式委託成交量": item["價差對單式委託成交量"]
+            }
+            data = make_influx_line('dlFutDataDown', tag_set, field_set, setReleasTime(item['交易日期']))
             requests.post('http://host.docker.internal:9001/telegraf', data=data.encode('utf-8'))
+            # requests.post('http://localhost:9001/telegraf', data=data.encode('utf-8'))
             return item
 
         return {}
--- taifex_scraper/spiders/dlFutDataDown.py
+++ taifex_scraper/spiders/dlFutDataDown.py
@@ -14,40 +14,54 @@ class DlfutdatadownSpider(scrapy.Spider):
     allowed_domains = ['taifex.com.tw']
 
     def start_requests(self):
-        # scrapy crawl -a start_month="2010/12" -a end_month="2011/3" dlFutDataDown
+        # scrapy crawl -a start_month="2010/12" -a end_month="2011/3" -a commodity_id="TE" dlFutDataDown
         # scrapyd-client schedule --arg start_month="2010/12" --arg end_month="2011/3" -p taifex_scraper dlFutDataDown
         if hasattr(self, 'start_month') and hasattr(self, 'end_month'):
-            return self._month_range_requests(self.start_month, self.end_month)
+            commodity_id = self.commodity_id if hasattr(self, 'commodity_id') else 'TX'
+            return self._month_range_requests(self.start_month, self.end_month, commodity_id)
 
         # scrapy crawl -a selected_date="2020/06/02" dlFutDataDown
         # scrapyd-client schedule --arg selected_date="2020/06/02" -p taifex_scraper dlFutDataDown
         selected_date = self.selected_date if hasattr(self, 'selected_date') else datetime.now().strftime('%Y/%m/%d')
-        return self._selected_date_request(selected_date)
+        commodity_id = self.commodity_id if hasattr(self, 'commodity_id') else 'TX'
+        return self._selected_date_request(selected_date, commodity_id)
     
     def parse_data(self, response):
         df = pd.read_csv(io.StringIO(response.text.replace(',\r\n','\r\n').replace(" ","")))
-        df = pd.DataFrame(df[["交易日期", "交易時段", "到期月份(週別)", "收盤價", "結算價"]])
 
-        # items = []
-        # for index, row in df.iterrows():
-        #     print(row['日期'], row['買權未平倉量'])
-        #     item = TaifexScraperItem()
-        #     item['date'] = row['日期']
-        #     item['oi'] = row['買權未平倉量']
-        #     # yield item
-        #     items.append(item)
+        # 交易日期
+        # 契約
+        # 到期月份(週別)
+        df['開盤價'] = df["開盤價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['最高價'] = df["最高價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['最低價'] = df["最低價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['收盤價'] = df["收盤價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['漲跌價'] = df["漲跌價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['漲跌%'] = df["漲跌%"].apply(lambda s: s.replace('%', '')).apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['成交量'] = df["成交量"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['結算價'] = df["結算價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['未沖銷契約數'] = df["未沖銷契約數"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['最後最佳買價'] = df["最後最佳買價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['最後最佳賣價'] = df["最後最佳賣價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['歷史最高價'] = df["歷史最高價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['歷史最低價'] = df["歷史最低價"].apply(lambda s: 0 if s == '-' else s).apply(lambda s: float(s))
+        df['是否因訊息面暫停交易'] = df["是否因訊息面暫停交易"].apply(lambda s: '' if s != s else s)
+        # 交易時段
+        df['價差對單式委託成交量'] = df["價差對單式委託成交量"].apply(lambda s: '' if s != s else s)
 
         items = df.to_dict(orient='records')
         
         return items
 
-    def _month_range_requests(self, start_month, end_month):
+    def _month_range_requests(self, start_month, end_month, commodity_id):
         reqs = []
 
         for m in pd.date_range(start_month, end_month, freq='MS').strftime("%Y/%m").tolist():
             data = {
                 'queryStartDate': first_date_of_month(m).strftime('%Y/%m/%d'), # '2019/03/20'
-                'queryEndDate': last_date_of_month(m).strftime('%Y/%m/%d')     # '2019/04/19'
+                'queryEndDate': last_date_of_month(m).strftime('%Y/%m/%d'),    # '2019/04/19'
+                'commodity_id': commodity_id,
+                'down_type': 1
             }
             req = Request(url='https://www.taifex.com.tw/cht/3/dlFutDataDown', 
                             method='POST', 
@@ -57,11 +71,11 @@ class DlfutdatadownSpider(scrapy.Spider):
             reqs.append(req)
         return reqs
 
-    def _selected_date_request(self, selected_date):
+    def _selected_date_request(self, selected_date, commodity_id):
         data = {
             'queryStartDate': selected_date, # '2020/6/3'
             'queryEndDate': selected_date,   # '2019/6/3'
-            'commodity_id': 'TX',
+            'commodity_id': commodity_id, # 'all', 'TX', 'MTX', 'TE', 'TF', etc. see https://www.taifex.com.tw/cht/3/dlFutDailyMarketView
             'down_type': 1
         }
         req = Request(url='https://www.taifex.com.tw/cht/3/dlFutDataDown', 

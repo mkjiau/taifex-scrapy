f03bde09ac012fc78c1809b90ee65c01b23ab1cb
Ming-Kai Jiau (AP15) <kasimjm7997@gmail.com>
2020-06-12 02:07:35 +0800

    fix: fix dlPcRatioDown according to the warnings from lint

--- taifex_scraper/spiders/dlPcRatioDown.py
+++ taifex_scraper/spiders/dlPcRatioDown.py
@@ -6,23 +6,26 @@ from datetime import datetime, timedelta
 import urllib
 import pandas as pd
 import io
-from taifex_scraper.items import *
+# from taifex_scraper.items import *
 from ..utils import first_date_of_month, last_date_of_month
 
 class DlpcratiodownSpider(Spider):
     name = 'dlPcRatioDown'
     allowed_domains = ['taifex.com.tw']
+    
+    selected_date = datetime.now().strftime('%Y/%m/%d')
+    start_month = None
+    end_month = None
 
     def start_requests(self):
         # scrapy crawl -a start_month="2010/12" -a end_month="2011/3" dlPcRatioDown
         # scrapyd-client schedule --arg start_month="2010/12" --arg end_month="2011/3" -p taifex_scraper dlPcRatioDown
-        if hasattr(self, 'start_month') and hasattr(self, 'end_month'):
-            return self._month_range_requests(self.start_month, self.end_month)
+        if self.start_month != None and self.end_month != None:
+            return self.__month_range_requests(self.start_month, self.end_month)
 
         # scrapy crawl -a selected_date="2020/06/02" dlPcRatioDown
         # scrapyd-client schedule --arg selected_date="2020/06/02" -p taifex_scraper dlPcRatioDown
-        selected_date = self.selected_date if hasattr(self, 'selected_date') else datetime.now().strftime('%Y/%m/%d')
-        return self._selected_date_request(selected_date)
+        return self.__selected_date_request(self.selected_date)
     
     def parse_data(self, response):
 
@@ -41,10 +44,11 @@ class DlpcratiodownSpider(Spider):
         
         return items
 
-    def _month_range_requests(self, start_month, end_month):
+    def __month_range_requests(self, start_month, end_month):
         reqs = []
 
-        for m in pd.date_range(start_month, end_month, freq='MS').strftime("%Y/%m").tolist():
+        for m in pd.date_range(start_month, end_month, freq='MS').tolist():
+            m = m.strftime("%Y/%m")
             data = {
                 'queryStartDate': first_date_of_month(m).strftime('%Y/%m/%d'), # '2019/03/20'
                 'queryEndDate': last_date_of_month(m).strftime('%Y/%m/%d')     # '2019/04/19'
@@ -57,7 +61,7 @@ class DlpcratiodownSpider(Spider):
             reqs.append(req)
         return reqs
 
-    def _selected_date_request(self, selected_date):
+    def __selected_date_request(self, selected_date):
         data = {
             'queryStartDate': selected_date, # '2020/6/3'
             'queryEndDate': selected_date    # '2019/6/3'
